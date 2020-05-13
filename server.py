f0416478732d5b0bafc95695f2513caae90bf166
CecyliaM <cecylia.meredith@gmail.com>
2020-05-13 15:23:28 +0200

    fixed answer display

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import connection
 import util
 
@@ -10,20 +10,19 @@ PATH = app.root_path
 @app.route("/list")
 def list():
     data = connection.get_data('question.csv', PATH)
+
     return render_template('list.html', data=data)
 
 
 @app.route("/question/<question_id>")
 def question(question_id):
     data = connection.get_data('question.csv', PATH)
-    question = []
     for i in data:
         if question_id == i[0]:
-            question.append(str(i))
+            question = i
+    answer= "koczkodan"
     #question = [i for i in data if question_id == i[0]]
-    print(question)
-    return render_template('question.html', question=question)
-
+    return render_template('question.html', question=question, question_id=question_id, answer=answer)
 
 
 @app.route("/add_question", methods=['POST', 'GET'])
@@ -45,11 +44,22 @@ def add_question():
     return render_template('add_question.html')
 
 
-@app.route("/question/<question_id>/new-answer")
-def add_answer():
-    data = connection.get_data('question.csv', PATH)
-    return render_template('add_answer.html', data=data)
-
+@app.route("/question/<question_id>/new-answer", methods=['POST', 'GET'])
+def add_answer(question_id):
+    if request.method == 'POST':
+        data = connection.get_data('answer.csv', PATH)
+        question_data = connection.get_data('question.csv', PATH)
+        question = [q for q in question_data if question_data[0] == question_id]
+        answer = request.form['answer']
+        answer_to_save = [question_id, answer]
+        connection.save_data(PATH, 'answer.csv', answer_to_save)
+        return render_template('question.html', answer_to_save=answer_to_save, data=data)
+    data = connection.get_data('answer.csv', PATH)
+    question_data = connection.get_data('question.csv', PATH)
+    print("hello")
+    question = "tyguhij"#[q for q in question_data if question_data[0] == question_id]
+    answer = "avshjk" #[q for q in data if data[0] == question_id]
+    return render_template('add_answer.html', question=question, answer=answer)
 
 @app.route("/question/<question_id>/delete")
 def delete():
