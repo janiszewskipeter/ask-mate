e8d30a0740264fbe1b3a0ba8f71bc7895e26c768
Grzegorz Norberciak <grzegorz.norberciak@gmail.com>
2020-05-14 13:08:35 +0200

    edit functionality added

--- connection.py
+++ connection.py
@@ -20,12 +20,15 @@ def get_data(FILENAME, PATH):
     return list_of_rows
 
 
-
-def save_data(PATH,FILENAME, data):
-    with open(PATH +'/sample_data/'+ FILENAME, 'a', newline='') as f:
+def save_data(PATH,FILENAME, data, mode):
+    with open(PATH +'/sample_data/'+ FILENAME, mode, newline='') as f:
         writer = csv.writer(f)
         writer.writerow(data)
 
-
+def save_edited_data(PATH,FILENAME, data, mode):
+    with open(PATH +'/sample_data/'+ FILENAME, mode, newline='') as f:
+        for row in data:
+            writer = csv.writer(f)
+            writer.writerow(row)
 
 
