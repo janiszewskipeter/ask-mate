2b80c4e1a674b7694519a25f0ffc472cdeaad152
CecyliaM <cecylia.meredith@gmail.com>
2020-05-26 13:19:30 +0200

    SQL setup and basic functions

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,25 @@
-'''
-Layer between the server and the data.
-Functions here should be called from the server.py
-and these should use generic functions from the connection.py
-'''
+from typing import List, Dict
+
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
+
+import sql_connection
+
+@sql_connection.connection_handler
+def get_questions(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM question
+        ORDER BY submission_time;"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def get_answers(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM answer
+        ORDER BY submission_time;"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+    """
